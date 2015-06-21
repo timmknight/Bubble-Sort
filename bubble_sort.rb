901e71763c45e039037b38bba47aa6b553391481
Tim Knight <timmilesknight@gmail.com>
2015-06-21 22:02:32 +0100

    messy solution to part 1

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,5 +1,22 @@
-bubble_sort([4,3,78,2,0,2])
 
 def bubble_sort(array)
-	
+	maxNumOfPass = array.length-1 
+	maxNumOfPass.times do
+
+	array.each_with_index do |num, index|
+		if index+1 < array.length
+			if num > array[index+1]
+				largerNum = num
+				smallerNum = array[index+1]
+
+				array[index] = smallerNum
+				array[index+1] = largerNum
+			end
+			end
+		end
+			puts "Bubble sort = #{array}"
+	end
+
 end
+
+bubble_sort([4,3,78,2,0,2])
