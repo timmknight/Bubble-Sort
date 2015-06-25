0db6c8bea18145110cc58a17e3ddb7231938491a
Tim Knight <timmilesknight@gmail.com>
2015-06-25 21:30:53 +0100

    finish project 2

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,4 +1,3 @@
-
 def bubble_sort(array)
 	maxNumOfPass = array.length-1 
 	maxNumOfPass.times do
@@ -14,9 +13,40 @@ def bubble_sort(array)
 			end
 			end
 		end
-			puts "Bubble sort = #{array}"
 	end
-
+	puts "Bubble sort = #{array}"
 end
 
 bubble_sort([4,3,78,2,0,2])
+# should return [0, 2, 2, 3, 4, 78]
+
+def bubble_sort_by(array)
+	maxNumOfPass = array.length-1 
+			index = 0
+	maxNumOfPass.times do
+
+		left = array[index]
+		right = array[index+1]
+
+			if (yield left,right) < 0
+				array[index] = right
+				array[index+1] = left
+			end
+
+				index +=1	
+
+			end
+	
+	puts "Bubble sort = #{array}"
+end
+
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right| 
+	 right.length - left.length
+end
+# Should return ["hi", "hey", "hello"]
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right| 
+	 left.length - right.length
+end
+# Should return ["hello", "hey", "hi"]
